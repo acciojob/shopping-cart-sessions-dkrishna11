3f09044b4bf8d2022c3d0415ac1f7f020af648db
dkrishna11 <116519809+dkrishna11@users.noreply.github.com>
2023-06-28 13:59:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -22,17 +22,59 @@ function renderProducts() {
 }
 
 // Render cart list
-function renderCart() {}
+function renderCart() {
+cartList.innerHTML = "";
+cart.forEach((cartItem) => {
+const li = document.createElement("li");
+li.innerHTML = ${cartItem.name} - $${cartItem.price} <button class="remove-from-cart-btn" data-id="${cartItem.id}">Remove</button>;
+cartList.appendChild(li);
+});
+
+// Update total price
+const cartTotal = cart.reduce((total, cartItem) => total + cartItem.price, 0);
+totalPrice.innerText = $${cartTotal};
+}
 
 // Add item to cart
-function addToCart(productId) {}
+function addToCart(productId) {
+const product = products.find((product) => product.id === productId);
+if (product) {
+cart.push(product);
+renderCart();
+}
+}
 
 // Remove item from cart
-function removeFromCart(productId) {}
+function removeFromCart(productId) {
+cart = cart.filter((cartItem) => cartItem.id !== productId);
+renderCart();
+}
 
 // Clear cart
-function clearCart() {}
+function clearCart() {
+cart = [];
+renderCart();
+}
 
 // Initial render
 renderProducts();
 renderCart();
+
+// Event listeners
+productList.addEventListener("click", (event) => {
+if (event.target.classList.contains("add-to-cart-btn")) {
+const productId = parseInt(event.target.dataset.id);
+addToCart(productId);
+}
+});
+
+cartList.addEventListener("click", (event) => {
+if (event.target.classList.contains("remove-from-cart-btn")) {
+const productId = parseInt(event.target.dataset.id);
+removeFromCart(productId);
+}
+});
+
+// Clear cart button
+const clearCartBtn = document.getElementById("clear-cart-btn");
+clearCartBtn.addEventListener("click", clearCart);
